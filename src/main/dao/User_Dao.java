fa71ba02d6d7ec373fa170d79d01d3c80f88a8fb
imade <jamoussiimad34@gmail.com>
2023-08-22 11:20:56 +0100

    tetst

--- src/main/dao/User_Dao.java
+++ src/main/dao/User_Dao.java
@@ -28,6 +28,32 @@ public class User_Dao {
         return user;
     }
 
+
+    public List<Users> selectionnerTousLesUsers() {
+        List<Users> usersList = new ArrayList<>();
+        String query = "SELECT * FROM users";
+        try (PreparedStatement preparedStatement = connection.prepareStatement(query);
+             ResultSet resultSet = preparedStatement.executeQuery()) {
+            while (resultSet.next()) {
+                int id = resultSet.getInt("id");
+                String nom = resultSet.getString("nom");
+                String prenom = resultSet.getString("prenom");
+                String email = resultSet.getString("email");
+                int age = resultSet.getint("age");
+                int getId_Role = resultSet.getint("getId_Role");
+
+                Etudiant users = new Users(id, nom, prenom,email,age, getId_Role);
+                usersList.add(users);
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return usersList;
+    }
+
+
+
+
     private Users createUserFromResultSet(ResultSet resultSet) throws SQLException {
         int id = resultSet.getInt("id");
         String firstName = resultSet.getString("prenom");
@@ -38,5 +64,39 @@ public class User_Dao {
 
         return new Users(id,lastName,firstName,email,age,role);
     }
+
+
+
+    private Users updateUser(Users users) throws SQLException {
+        String  query = "UPDATE users set nom=? , prenom=? , email=? , age=? , Id_Role=?  WHERE id = ?";
+        try(Connection connection = connectionManager.getConnection()){
+            PreparedStatement preparedStatement = connection.prepareStatement(query);
+            preparedStatement.setString(1, etudiant.getNom());
+            preparedStatement.setString(2, etudiant.getPrenom());
+            preparedStatement.setString(3, etudiant.getEmail());
+            preparedStatement.setString(4, etudiant.getId_Role());
+            preparedStatement.setString(5, etudiant.getId());
+            System.out.println(preparedStatement);
+            preparedStatement.executeUpdate();
+        }
+        return users;
+    }
+
+
+    public void supprimerUsers(int id) {
+        String query = "DELETE FROM users WHERE id = ?";
+        try (PreparedStatement preparedStatement = connection.prepareStatement(query)) {
+            preparedStatement.setInt(1, id);
+            preparedStatement.executeUpdate();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+
+
+
+
+
 
 }
